917295adbb8c52ffb15ab37ab56717a3d6cfeb4d
lomadi <lomadi@mac.com>
2016-10-31 01:42:18 +0100

    minor update

--- commands-repository.sh
+++ commands-repository.sh
@@ -2,6 +2,19 @@ docker ps -aq
 
 docker rm  $(docker ps -aq)
 
-
-
 docker build -t lomadi/webtrees .
+docker build -t lomadi/webtrees  images/webtrees
+
+docker run -it --rm --name webtrees  lomadi/webtrees
+
+docker-compose up -d
+docker-compose down
+
+docker logs webtrees
+docker exec -it webtrees /bin/bash
+
+
+use webtrees;
+show tables;
+
+select * from wt_user ;
